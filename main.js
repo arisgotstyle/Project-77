13a0ee0e2d47b9964ada5a23e6ed398ed7c1376e
arisgotstyle <77303361+arisgotstyle@users.noreply.github.com>
2021-03-30 19:58:27 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,27 @@
+var name_1="Aritra ";
+
+var name_2="Maitra";
+
+var full_name=name_1.concat(name_2);
+
+console.log(full_name);
+
+var number_1="12";
+
+var number_2="4";
+
+var add=Math.add(number_1+number_2);
+
+console.log(add);
+
+var subtract=Math.subtract(number_1,number_2);
+
+console.log(subtract)
+
+var multiply=Math.multiply(number_1,number_2);
+
+console.log(multiply);
+
+var divide=Math.divide(number_1,number_2);
+
+console.log(divide);
